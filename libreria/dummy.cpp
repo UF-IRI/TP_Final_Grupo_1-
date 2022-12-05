8a8773cee847ec2c747088da016c1495608a3f77
Lucas Iglesias <LucasG_iglesias@hotmail.com>
2022-12-05 09:37:30 -0300

    Elimine verificar 10 años del dummy, redundaba

--- libreria/dummy.cpp
+++ libreria/dummy.cpp
@@ -8,22 +8,6 @@ namespace foobar
     };
 }
 
-bool verificar_tiempo_10años(Historia_clinica pac_hist)
-{
-			time_t actual;
-			time(*actual);
-			time_t fecha_ultimo;
-			fecha_ultimo = pac_hist.fecha_ultima_cita.Anio * 31540000 + pac_hist.fecha_ultima_cita.mes * 262800 + pac_hist.fecha_ultima_cita.dia * 86400;
-			float decada;
-			decada = difftime(actual, fecha_ultimo) / 31540000;
-			if (decada >= 10)
-			{
-				return true;
-			}
-			else
-				return false;
-
-}
 void agregar_un_paciente_al_archivo(Historia_clinica paciente) //agrega un paciente al archivo de irrecuperables
 {
 	fstream fp;
